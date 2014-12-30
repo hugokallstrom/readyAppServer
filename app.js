2004021017981fa8d1a70377f55c1b244e13a09f
oi11hkm <oi11hkm@cs.umu.se>
2014-12-30 15:39:44 +0100

    test post

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var restify = require('restify');
+var restifyOauth2 = require('restify-oauth2');
 var user = require('./lib/user.js');
 var friends = require('./lib/friends.js');
 
@@ -6,11 +7,14 @@ var friends = require('./lib/friends.js');
 var server = restify.createServer({
 	name: 'ReadyApp',
 });
+
 server.use(restify.bodyParser());
+server.use(restify.authorizationParser());
 
 // Routes
 server.get('/user/:userId', user.getUser);
 server.post('/user/:userId', user.register);
+server.post('/post', user.test)
 server.post('/register', user.register);
 server.get('/friends/:userId', friends.getFriendList)
 server.post('/friends/:userId', friends.addFriend)
