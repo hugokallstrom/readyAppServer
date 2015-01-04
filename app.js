b6790b4b1692567a42f1f9a73d28c92bdb115b77
oi11hkm <oi11hkm@cs.umu.se>
2015-01-04 14:17:45 +0100

    search added, ip updated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var user = require('./lib/user.js');
 var friends = require('./lib/friends.js');
 var passport = require('passport');
 var auth = require('./lib/auth.js');
+var ip = require('./lib/ip.js');
 
 var server = restify.createServer({
 	name: 'ReadyApp',
@@ -12,15 +13,15 @@ server.use(passport.initialize());
 server.use(restify.bodyParser());
 server.use(restify.authorizationParser());
 
-// Routes
 server.post('/login', auth.isAuthenticated, user.authenticate);
 server.get('/user/:userId', auth.isAuthenticated, user.getUser);
 server.post('/user/:userId', auth.isAuthenticated, user.register);
 server.post('/register', user.register);
-server.get('/friends/:userId', auth.isAuthenticated, friends.getFriendList)
-server.post('/friends/:userId/:friendId', auth.isAuthenticated, friends.addFriend)
-
-
+server.get('/friends/:userId', auth.isAuthenticated, friends.getFriendList);
+server.post('/friends/:userId/:friendId', auth.isAuthenticated, friends.addFriend);
+server.post('/ip/:userId', auth.isAuthenticated, ip.addIp);
+server.get('/ip/:userId', auth.isAuthenticated, ip.getIp);
+server.get('/search/:userId', auth.isAuthenticated, user.search);
 
 server.listen(process.env.PORT || 8080, function() {
 	console.log('%s listening at %s', server.name, server.url);
